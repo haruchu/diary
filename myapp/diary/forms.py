78276fee1b8d21576af2552995f01e9445b3a1b7
haru <haruki6180@gmail.com>
2021-06-27 15:30:26 +0900

    textを改行ありで

--- myapp/diary/forms.py
+++ myapp/diary/forms.py
@@ -3,7 +3,7 @@ from .models import Diary
 
 class DiaryForm(forms.ModelForm):
     text = forms.CharField(widget=forms.Textarea(
-        attrs={'cols': '40', 'rows': '5', 'max_length': '200'}))
+        attrs={'cols': '40', 'rows': '5','wrap':"soft", 'max_length': '200'}))
 
     class Meta:
         model = Diary
